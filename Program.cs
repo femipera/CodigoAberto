079cd2e8dd1618312f783fad212f9ddf0fd4dbaa
femipera <figueiredo@outlook.com>
2021-10-03 11:02:38 -0300

    Projeto inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteParaOGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
